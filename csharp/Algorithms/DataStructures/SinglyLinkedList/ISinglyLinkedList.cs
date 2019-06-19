0c107df636a4cce165da60df4b2509b5d27e9102
kenneth wilson <kennysquaredonline@gmail.com>
2019-06-19 08:58:18 +0100

    Add stack and singlylinkedlist from github collections project

---
+++ csharp/Algorithms/DataStructures/SinglyLinkedList/ISinglyLinkedList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace RiskAndPricingSolutions.Algorithms.DataStructures.SinglyLinkedList
+{
+    public interface ISinglyLinkedList<T> : IEnumerable<T>
+    {
+        void AddFirst(T data);
+
+        void AddLast(T data);
+
+        void RemoveFirst();
+
+        void RemoveLast();
+
+        int Count { get; }
+
+        bool Contains(T value);
+
+        bool Remove(T value);
+    }
+}
---
+++ csharp/Algorithms/DataStructures/Stack/IStack.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RiskAndPricingSolutions.Algorithms.DataStructures.Stack
+{
+    public interface IStack<T>
+    {
+        void Push(T item);
+
+        T Pop();
+
+        long Count { get; }
+    }
+}
